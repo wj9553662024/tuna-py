0edaf91fae5ac7d5710edc967f738d8c49a321da
caodonglei <donglei.cao@chainup.com>
2026-02-03 00:53:02 +0800

    MOD: binance public ws

---
+++ tunapy/quote/bn_public_ws.py
@@ -0,0 +1,87 @@
+""" Binance Quote on WS
+"""
+import os
+import time
+import ujson
+
+from binance.websocket.spot.websocket_stream import SpotWebsocketStreamClient
+from common.utils.redis_client import DATA_REDIS_CLIENT
+from otcopus.utils.log_util import create_logger
+
+
+# one munite = 600 * 100 ms
+ONE_MIN_HUNDRED_MS = 600
+# binance spot partial depth
+EXCHANGE_DEPTH_PREFIX = 'bnsd'
+# binance spot ticker
+EXCHANGE_TICKER_PREFIX = 'bnst'
+
+CURR_PATH = os.path.dirname(os.path.abspath(__file__))
+LOGGER = create_logger(os.path.join(CURR_PATH, 'log'), "bn_pubws.log", "BN-PUBWS", 10)
+
+
+def _key(tag, ts):
+    """ BiNance Spot
+        The update period of Binance WS is 100ms.
+        1 minutes have 60 * 10 = 600 (100ms)
+    """
+    return f'{EXCHANGE_DEPTH_PREFIX}{tag}{ts % ONE_MIN_HUNDRED_MS}'
+
+
+def _handle_orderbook_depth(rkey: str, req_ts: int, data: dict) -> dict:
+    order_book = {
+        'asks': sorted([(float(a), float(q)) for a, q in data['asks']], key=lambda x: x[0]),
+        'bids': sorted([(float(b), float(q)) for b, q in data['bids']], key=lambda x: x[0],
+                       reverse=True),
+    }
+    DATA_REDIS_CLIENT.set_dict(f'{rkey}_value', order_book)
+    DATA_REDIS_CLIENT.set_int(rkey, req_ts)
+    LOGGER.info('Update Depth %s, ask size=%d, bid size=%s',
+                rkey, len(order_book['asks']), len(order_book['bids']))
+    return order_book
+
+def _handle_ticker(data: dict, req_ts: int) -> dict:
+    symbol = data['data']['s']
+    price = float(data['data']['p'])
+    qty = float(data['data']['q'])
+
+    rkey = f'{EXCHANGE_TICKER_PREFIX}{symbol}{req_ts % ONE_MIN_HUNDRED_MS}'
+    DATA_REDIS_CLIENT.set_dict(f'{rkey}_value', {'price': price, 'qty': qty})
+    DATA_REDIS_CLIENT.set_int(rkey, req_ts)
+    LOGGER.info('Update Tick %s, price=%s, qty=%s', symbol, price, qty)
+
+def message_handler(_, message):
+    ''' thread and message
+    '''
+    message = ujson.loads(message)
+    if 'stream' in message:
+        req_ts = int(10 * time.time())
+        if 'depth' in message['stream']:
+            pair, _, _ = message['stream'].split('@')
+            rkey = _key(pair, req_ts)
+            # order book partial depth
+            _handle_orderbook_depth(rkey, req_ts, message['data'])
+
+        elif 'aggTrade' in message['stream']:
+            _handle_ticker(message, req_ts)
+
+
+def error_handler(_, message):
+    LOGGER.error(message)
+
+def bn_subscribe(depth_symbols: list[str], ticker_symbols: list[str]):
+    """ subscribe partial depth or ticker of given symbols
+    """
+    ws = SpotWebsocketStreamClient(on_message=message_handler, on_error=error_handler,
+                                   is_combined=True)
+    topics = []
+    for symbol in depth_symbols:
+        symbol = symbol.lower()
+        topics.append(f'{symbol}@depth20@100ms')
+    for symbol in ticker_symbols:
+        topics.append(f'{symbol}@aggTrade')
+
+    ws.subscribe(stream=topics)
+
+    while 1:
+        time.sleep(1)
--- tunapy/quote/market_main.py
+++ tunapy/quote/market_main.py
@@ -9,110 +9,60 @@ BASE_DIR = os.path.dirname(CURR_DIR)
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
-import common.jpm_config as jpm_config
-from common.configer import Configer
-from common.utils.log_utils import create_logger
-from quote.bn_public_ws import create_market_data as bn_pubws
-from quote.gate_public_ws import create_market_data as gate_pubws
-from quote.jc_public_ws import create_market_data as jc_pubws
-from quote.mexc_public_ws import create_market_data as mexc_pubws
-from quote.bitmart_public_ws import create_market_data as bitmart_pubws
-from quote.gate_alpha_quote import alpha_quote
+from management.market_making import TokenParameter as MakerParameter
+from management.self_trade import TokenParameter as SelftradeParameter
+from quote.bn_public_ws import bn_subscribe
+from quote.okx_public_ws import okx_subscribe
+
+EXCHANGE_BN = 'Binance'
+EXCHANGE_OKX = 'OKX'
 
 
-def _create_bn_ws(tokens: list):
-    topics, log_name = [], 'BNSPOT-PubWS'
-    for symbol in tokens:
-        symbol = symbol.lower()
-        topics.append(f'{symbol}@depth20@100ms')
-        if jpm_config.COMBINE_USDS:
-            if symbol[-1] == 't':
-                topics.append(f'{symbol[:-1]}c@depth20@100ms')
-            else:
-                topics.append(f'{symbol[:-1]}t@depth20@100ms')
-        topics.append(f'{symbol}@aggTrade')
-    logger = create_logger(CURR_DIR, "quoter.log", log_name, backup_cnt=10)
-    logger.info('Topcis: %s', topics)
-    bn_pubws(topics)
-
-def _create_gate_ws(tokens: list):
-    """ create market client subscribing public websocket
-    """
-    log_name = 'GATESPOT-PubWS'
-    topics = [symbol.upper() for symbol in tokens]
-    logger = create_logger(CURR_DIR, "quoter.log", log_name, backup_cnt=10)
-    logger.info('Topcis: %s', topics)
-    gate_pubws(topics)
-
-def _create_jc_ws(tokens: dict):
-    """ create market client subscribing public websocket
-    """
-    log_name = 'JUSPOT-PubWS'
-    topics = list(tokens.keys())
-    logger = create_logger(CURR_DIR, "quoter.log", log_name, backup_cnt=10)
-    logger.info('Topcis: %s', topics)
-    jc_pubws(topics, logger)
-
-def _create_mexc_ws(tokens: list):
-    """ create market client subscribing public websocket
-    """
-    log_name = 'MEXCSPOT-PubWS'
-    topics = [symbol.upper() for symbol in tokens]
-    logger = create_logger(CURR_DIR, "quoter.log", log_name, backup_cnt=10)
-    logger.info('Topcis: %s', topics)
-    mexc_pubws(topics)
-
-def _create_bitmart_ws(tokens: list):
-    """ create market client subscribing public websocket
-    """
-    log_name = 'BITMARTSPOT-PubWS'
-    topics = [symbol.upper() for symbol in tokens]
-    logger = create_logger(CURR_DIR, "quoter.log", log_name, backup_cnt=10)
-    logger.info('Topcis: %s', topics)
-    bitmart_pubws(topics)
-
-def _create_alpha_rest(tokens: dict):
-    """ create gate alpha market client polling tickers
-    """
-    log_name = 'GateAlpha-Rest'
-    logger = create_logger(CURR_DIR, "quoter.log", log_name, backup_cnt=10)
-    logger.info('Tokens: %s', tokens)
-    alpha_quote(tokens, logger)
-
-
-def main():
+def main(exchange, maker_params: list[MakerParameter], selftrade_params: list[SelftradeParameter]):
     """ main workflow of market data
-        symbols.append('jasmyusdt@depth20@100ms')
-        symbols.append('jasmyusdt@aggTrade')
     """
-    if len(sys.argv) != 2:
-        print(f'python3 {sys.argv[0]} <APP_NAME>')
-        return
-
-    app_name = sys.argv[1]
-    configer = Configer(app_name)
-    quote_config = configer.get_config()
-
-    if jpm_config.MAIN_QUOTE_EXCHANGE == jpm_config.EXCHANGE_BN:
-        _create_bn_ws(quote_config['TOKENS'])
-    elif jpm_config.MAIN_QUOTE_EXCHANGE == jpm_config.EXCHANGE_GATE:
-        _create_gate_ws(quote_config['TOKENS'])
-    elif jpm_config.MAIN_QUOTE_EXCHANGE == jpm_config.EXCHANGE_MEXC:
-        _create_mexc_ws(quote_config['TOKENS'])
-    elif jpm_config.MAIN_QUOTE_EXCHANGE == jpm_config.EXCHANGE_BITMART:
-        _create_bitmart_ws(quote_config['TOKENS'])
-    elif jpm_config.MAIN_QUOTE_EXCHANGE == jpm_config.EXCHANGE_OKX:
-        log_name = 'OKXSPOT-PubWS'
-    elif jpm_config.MAIN_QUOTE_EXCHANGE == jpm_config.EXCHANGE_JU:
-        _create_jc_ws(quote_config['TOKENS'])
-    elif jpm_config.MAIN_QUOTE_EXCHANGE == jpm_config.EXCHANGE_GATE_ALPHA:
-        _create_alpha_rest(quote_config['TOKENS'])
+    
+    maker_symbols = [param.follow_symbol for param in maker_params]
+    selftrade_symbols = [param.follow_symbols for param in selftrade_params]
+    
+    if exchange == EXCHANGE_BN:
+        bn_subscribe(maker_symbols, selftrade_symbols)
+    elif exchange == EXCHANGE_OKX:
+        okx_subscribe(maker_symbols, selftrade_symbols)
     else:
         return
 
-    #get_market_client(MAIN_QUOTE_EXCHANGE, quote_config['TOKENS'])
-    #get_market_client(BACKUP_QUOTE_EXCHANGE, topics)
 
 
 if __name__ == '__main__':
-    main()
+    maker_params = [
+        MakerParameter({
+            'follow_symbol': 'BTCUSDT',
+            'maker_symbol': 'btc_usdt',
+            'price_decimals': 2,
+            'qty_decimals': 5,
+        }),
+        MakerParameter({
+            'follow_symbol': 'ETHUSDT',
+            'maker_symbol': 'eth_usdt',
+            'price_decimals': 2,
+            'qty_decimals': 4,
+        }),
+    ]
+
+    selftrade_params = [
+        SelftradeParameter({
+            'follow_symbol': 'BTCUSDT',
+            'maker_symbol': 'btc_usdt',
+            'price_decimals': 2,
+            'qty_decimals': 5,
+        }),
+        SelftradeParameter({
+            'follow_symbol': 'ETHUSDT',
+            'maker_symbol': 'eth_usdt',
+            'price_decimals': 2,
+            'qty_decimals': 4,
+        })
+    ]
+    
+    main(EXCHANGE_BN, maker_params, selftrade_params)
